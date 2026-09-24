17e99c30bccb0e6fda752f4954dafa25892189cd
ffikri604 <ffikri604@gmail.com>
2026-09-24 07:40:17 +0000

    Push

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import Translator from './utilities/Translator'
 
 export default function Layout() {
   return (
